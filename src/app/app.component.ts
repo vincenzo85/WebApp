7ebde51d463e953588b6e96c4ba9971f840dcfb5
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-02-15 11:59:38 +0100

    ecco i vari file del progetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 // questi sono i "fogli" del nostro component
 @Component({
+  // selector... sarà il tag del mi html dell'index
+  
   selector: 'app-root',
+  // html che ci voglio mettere..
+
   templateUrl: './app.component.html',
+  // css del progetto... anche se usando bootstrap... css non ne metto tantissimo...
+
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
